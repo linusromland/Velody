99e43cf630e342eb600afab54b0a28f4214e6004
Linus Romland <romlandlinus@gmail.com>
2021-09-14 18:11:42 +0200

    added queue command

---
+++ bot/commands/queue.js
@@ -0,0 +1,22 @@
+//Dependencies Import
+const {
+    MessageEmbed
+} = require("discord.js");
+const voice = require("../voice.js");
+
+module.exports = {
+    slash: "both",
+    testOnly: false,
+    description: "Shows queue - Velody",
+    aliases: ['q'],
+    callback: async ({
+        interaction,
+        client,
+        args
+    }) => {
+        let embed = new MessageEmbed();
+        embed.setAuthor('Velody', 'https://raw.githubusercontent.com/linusromland/Velody/master/Velody-logos.jpeg', 'https://github.com/linusromland/Velody')
+        embed = voice.queue(embed);
+        return embed;
+    },
+};
