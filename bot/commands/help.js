f1dbaad8ecd338ee37e952c8f0b9e73878d71664
Linus Romland <romlandlinus@gmail.com>
2021-09-14 18:10:42 +0200

    new help commands

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,19 +1,27 @@
 //Dependencies Import
-const { MessageEmbed } = require("discord.js");
+const {
+  MessageEmbed
+} = require("discord.js");
 
 module.exports = {
   slash: "both",
   testOnly: false,
   description: "Shows available commands on Velody",
-  callback: async ({ interaction, args }) => {
+  callback: async ({
+    interaction,
+    args
+  }) => {
     //Creates a messageEmbed for reply
     const embed = new MessageEmbed();
     embed.setAuthor('Velody', 'https://raw.githubusercontent.com/linusromland/Velody/master/Velody-logos.jpeg', 'https://github.com/linusromland/Velody')
     embed.setTitle("Available Commands"); //Sets the title for the Embed
     embed.setDescription(
-      `"/join" - Joins to voice channel\n
-      "/leave" - Leaves voice channel\n
-      "/play <song>" - Plays song from URL or search\n`
+      `"/join" - Joins to voice channel\u200b
+      "/leave" - Leaves voice channel\u200b
+      "/play <song>" - Plays song from URL or search\u200b
+      "/nowplaying" - Shows what is currently playing\u200b
+      "/queue" - Shows queue\u200b
+      "/skip" - Skips current song`
     );
     return embed;
   },
