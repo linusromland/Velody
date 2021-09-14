6078bafc6ae0b7b0fe855471412b47d3c5c8be04
Linus Romland <romlandlinus@gmail.com>
2021-09-14 18:11:48 +0200

    added skip

--- bot/commands/skip.js
+++ bot/commands/skip.js
@@ -1,23 +1,24 @@
 //Dependencies Import
 const {
-    MessageEmbed
-  } = require("discord.js");
-  const voice = require("../voice.js");
-  
-  module.exports = {
-    slash: "both",
-    testOnly: true,
-    description: "Skips current song!",
-    callback: async ({
-      interaction,
-      client
-    }) => {
-      let embed = new MessageEmbed();
-      embed.setAuthor('Velody', 'https://raw.githubusercontent.com/linusromland/Velody/master/Velody-logos.jpeg', 'https://github.com/linusromland/Velody')
-      embed.setTitle("Something went wrong!");
-  
-      embed = voice.skip(embed);
-  
-      return embed;
-    },
-  };
+  MessageEmbed
+} = require("discord.js");
+const voice = require("../voice.js");
+
+module.exports = {
+  slash: "both",
+  testOnly: false,
+  description: "Skips current song!",
+  aliases: ['s'],
+  callback: async ({
+    interaction,
+    client
+  }) => {
+    let embed = new MessageEmbed();
+    embed.setAuthor('Velody', 'https://raw.githubusercontent.com/linusromland/Velody/master/Velody-logos.jpeg', 'https://github.com/linusromland/Velody')
+    embed.setTitle("Something went wrong!");
+
+    embed = voice.skip(embed);
+
+    return embed;
+  },
+};
