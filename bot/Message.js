e29a9ac84c4c6abd4c1c86f04377ff0664883d84
Linus Romland <romlandlinus@gmail.com>
2021-09-17 08:22:13 +0200

    Created default template, Message.js

---
+++ bot/Message.js
@@ -0,0 +1,8 @@
+//Dependencies Import
+const discordJS = require("discord.js");
+
+let message = new discord.MessageEmbed();
+message.setAuthor('Velody', 'https://raw.githubusercontent.com/linusromland/Velody/master/assets/logo.jpeg', 'https://github.com/linusromland/Velody')
+
+
+module.exports = Customer;
