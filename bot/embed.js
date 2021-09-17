46b1804f56abc1fb39daef6b0559246b5a1e8471
Linus Romland <romlandlinus@gmail.com>
2021-09-17 17:18:31 +0200

    I'm a dum dum

--- bot/embed.js
+++ bot/embed.js
@@ -2,7 +2,7 @@ exports.setDefaults = (message) => {
     message.setAuthor('Velody', 'https://raw.githubusercontent.com/linusromland/Velody/master/assets/logo.jpeg', 'https://github.com/linusromland/Velody')
 }
 
-exports.setError = (message) {
+exports.setError = (message) => {
     message.setTitle("Something went wrong!");
     message.setDescription("If the issue persists, please add an issue to GitHub!")
     message.setURL("https://github.com/linusromland/Velody/issues/new")
