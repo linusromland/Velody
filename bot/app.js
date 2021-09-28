03e73755c9de11689244e995a1a2b1b37812d194
Linus Romland <romlandlinus@gmail.com>
2021-09-28 13:18:59 +0200

    added custom prefix from .env

--- bot/app.js
+++ bot/app.js
@@ -7,7 +7,7 @@ require("better-logging")(console);
 //Dependencies import
 const DiscordJS = require("discord.js");
 const WOKCommands = require("wokcommands");
-
+const prefix = process.env.PREFIX || "!"
 
 //Local Variables
 const guildId = "554977304665784325";
@@ -19,11 +19,15 @@ const client = new DiscordJS.Client({
 client.on("ready", () => {
 
   console.info(`Logged in as ${client.user.tag}!`);
-  new WOKCommands(client, {
-    commandsDir: "commands",
-    testServers: [guildId],
-    showWarns: false,
-  });
+  let wok = new WOKCommands(client, {
+      commandsDir: "commands",
+      testServers: [guildId],
+      showWarns: false,
+    })
+    .setDefaultPrefix(prefix)
+    .setColor(0xff0000)
+    .setBotOwner("400691601011113986") //sets bot owner to linusromland#7577
+  console.info(`WOKCommands loaded and using prefix "${prefix}"`)
 });
 
 
