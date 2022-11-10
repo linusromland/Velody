03da04cc59db1d6b400e4d2c49e30c9b9007e6f4
Linus Romland <48645689+linusromland@users.noreply.github.com>
2022-11-10 09:52:23 +0100

    start init of discordjs

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,42 @@
-console.log('Hello World');
+import { Interaction } from 'discord.js';
+
+const { REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const commands = [
+	{
+		name: 'ping',
+		description: 'Replies with Pong!'
+	}
+];
+
+const { TOKEN, CLIENT_ID } = process.env;
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction: Interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(TOKEN);
