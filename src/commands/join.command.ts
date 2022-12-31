407b36f16ba30e142078750886db3b79beb58841
Linus Romland <hello@linusromland.com>
2022-12-31 17:21:47 +0100

    Improved join command

--- src/commands/join.command.ts
+++ src/commands/join.command.ts
@@ -6,6 +6,8 @@ import { GuildMember, Message, VoiceBasedChannel } from 'discord.js';
 
 // Internal dependencies
 import Server from '../classes/Server';
+import servers from '../utils/servers';
+import Embed from '../classes/Embed';
 
 export class JoinCommand extends Command {
 	public constructor(context: Command.Context, options: Command.Options) {
@@ -21,17 +23,38 @@ export class JoinCommand extends Command {
 	}
 
 	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+		const embed: Embed = new Embed();
+		embed.loading();
 		const msg: Message<boolean> = (await interaction.reply({
-			content: 'Loading...',
+			embeds: [embed.embed],
 			fetchReply: true
 		})) as Message;
 
 		const channel: VoiceBasedChannel | null = (interaction?.member as GuildMember)?.voice?.channel;
+		console.log(channel);
 
 		if (isMessageInstance(msg) && channel) {
-			const server: Server = new Server(channel);
+			let server: Server = servers.get(interaction.guildId as string) as Server;
 
-			await msg.edit('Help menu');
+			if (!server) {
+				server = new Server(channel, interaction.guildId as string);
+				servers.add(server);
+			}
+
+			if (server.connectedChannelId === channel.id) {
+				embed.setTitle('Already connected to `' + channel.name + '`');
+				embed.setDescription('Use `/play <song>` to play a song');
+				return msg.edit({ embeds: [embed.embed] });
+			}
+
+			server.join(channel);
+			embed.setTitle('Joined `' + channel.name + '`');
+			embed.setDescription('Use `/play <song>` to play a song');
+			msg.edit({ embeds: [embed.embed] });
+		} else {
+			embed.setTitle('You are not connected to a voice channel');
+			embed.setDescription('Join a voice channel and try again');
+			msg.edit({ embeds: [embed.embed] });
 		}
 	}
 }
