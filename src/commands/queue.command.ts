95e190cffd69e43a8eca128522e127f37ea741a7
Linus Romland <hello@linusromland.com>
2023-01-03 09:28:20 +0100

    Added queue command

---
+++ src/commands/queue.command.ts
@@ -0,0 +1,85 @@
+// External dependencies
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { isMessageInstance } from '@sapphire/discord.js-utilities';
+import { ChatInputCommand, Command } from '@sapphire/framework';
+import { GuildMember, Message, VoiceBasedChannel } from 'discord.js';
+
+// Internal dependencies
+import Server from '../classes/Server';
+import servers from '../utils/servers';
+import Embed from '../classes/Embed';
+import Video from '../interfaces/Video';
+import formatTime from '../utils/formatTime';
+
+export class QueueCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, {
+			...options,
+			name: 'queue',
+			description: 'Shows the current queue'
+		});
+	}
+
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder: SlashCommandBuilder) =>
+			builder.setName(this.name).setDescription(this.description)
+		);
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+		const embed: Embed = new Embed();
+		embed.loading();
+		const msg: Message<boolean> = (await interaction.reply({
+			embeds: [embed.embed],
+			fetchReply: true
+		})) as Message;
+
+		const channel: VoiceBasedChannel | null = (interaction?.member as GuildMember)?.voice?.channel;
+
+		if (!isMessageInstance(msg)) return;
+
+		if (channel) {
+			const server: Server = servers.get(interaction.guildId as string) as Server;
+
+			if (!server) {
+				embed.setTitle('Not connected to a voice channel');
+				embed.setDescription('Use `/join` to connect to a voice channel');
+				return msg.edit({ embeds: [embed.embed] });
+			}
+
+			const queue: Video[] = server.queue;
+
+			if (queue && queue.length > 0) {
+				embed.setTitle('Current queue');
+				let description: string = `__Now Playing:__
+        [${queue[0].title}](${queue[0].url}) - ${formatTime(queue[0].length)}
+        \n`;
+
+				if (queue.length > 1) {
+					description += '__Up next:__\n';
+					for (let i: number = 1; i < queue.length; i++) {
+						description +=
+							'``' + i + '.``' + ` [${queue[i].title}](${queue[i].url}) - ${formatTime(queue[i].length)}\n`;
+					}
+				} else {
+					description += 'No more songs in queue';
+				}
+
+				embed.setDescription(description);
+			} else {
+				embed.setTitle('No songs in queue');
+				embed.setDescription('Use `/play` to add songs to the queue');
+			}
+
+			msg.edit({ embeds: [embed.embed] });
+		} else {
+			embed.setTitle('You are not connected to a voice channel');
+			embed.setDescription('Join a voice channel and try again');
+			msg.edit({ embeds: [embed.embed] });
+		}
+
+		this.container.logger.info(
+			`User ${interaction?.user?.tag}(${interaction?.user?.id}) requested the bot to show currently playing on server ${interaction.guild?.name}(${interaction.guildId})`
+		);
+	}
+}
--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,16 +1,18 @@
-const formatTime = (total: number, current: number) => {
+const formatTime = (total: number, current?: number) => {
 	// Format time as 00:00 / 00:00. If the total time is over 1 hour, format it as 00:00:00 / 00:00:00
 	const totalHours: number = Math.floor(total / 3600);
 	const totalMinutes: number = Math.floor((total % 3600) / 60);
 	const totalSeconds: number = Math.floor(total % 60);
 
-	const currentHours: number = Math.floor(current / 3600);
-	const currentMinutes: number = Math.floor((current % 3600) / 60);
-	const currentSeconds: number = Math.floor(current % 60);
-
 	const totalFormatted: string = `${totalHours > 0 ? `${totalHours < 10 ? `0${totalHours}` : totalHours}:` : ''}${
 		totalMinutes < 10 ? `0${totalMinutes}` : totalMinutes
 	}:${totalSeconds < 10 ? `0${totalSeconds}` : totalSeconds}`;
+
+	if (!current) return totalFormatted;
+
+	const currentHours: number = Math.floor(current / 3600);
+	const currentMinutes: number = Math.floor((current % 3600) / 60);
+	const currentSeconds: number = Math.floor(current % 60);
 
 	const currentFormatted: string = `${
 		totalHours > 0 ? `${currentHours < 10 ? `0${currentHours}` : currentHours}:` : ''
