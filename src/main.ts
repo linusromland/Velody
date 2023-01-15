7f633a187a890e29b13ec9870a903670e0ec5363
Linus Romland <hello@linusromland.com>
2023-01-15 15:03:46 +0100

    temporary test

--- src/main.ts
+++ src/main.ts
@@ -13,26 +13,26 @@ const client: SapphireClient<boolean> = new SapphireClient({
 
 console.log('bottoken: ' + bottoken);
 
-client.login(bottoken);
+// client.login(bottoken);
 
-client.on('ready', () => {
-	container.logger.info(`Connected as ${client.user?.tag}`);
-});
+// client.on('ready', () => {
+// 	container.logger.info(`Connected as ${client.user?.tag}`);
+// });
 
-//On bot being kicked from voice channel
-client.on('voiceStateUpdate', (oldState: VoiceState, newState: VoiceState) => {
-	if (oldState.channel && !newState.channel) {
-		servers.remove(oldState.guild.id);
-		container.logger.info(
-			`Bot was kicked from voice channel ${oldState.channel.name}(${oldState.channel.id}) on server ${oldState.guild.name}(${oldState.guild.id})`
-		);
-	}
-});
+// //On bot being kicked from voice channel
+// client.on('voiceStateUpdate', (oldState: VoiceState, newState: VoiceState) => {
+// 	if (oldState.channel && !newState.channel) {
+// 		servers.remove(oldState.guild.id);
+// 		container.logger.info(
+// 			`Bot was kicked from voice channel ${oldState.channel.name}(${oldState.channel.id}) on server ${oldState.guild.name}(${oldState.guild.id})`
+// 		);
+// 	}
+// });
 
-function shutdown() {
-	client.destroy();
-	process.exit(0);
-}
+// function shutdown() {
+// 	client.destroy();
+// 	process.exit(0);
+// }
 
-process.on('SIGTERM', shutdown);
-process.on('SIGINT', shutdown);
+// process.on('SIGTERM', shutdown);
+// process.on('SIGINT', shutdown);
